faab43cff6f6dd56ce4b4dc301c8b5b4d8cebcab
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-20 19:51:02 +0530

    Added the shipping script

---
+++ rabbitmq.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$LOGFILE-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ..... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ..... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "Please run as root user"
+    exit 1
+else
+    echo -e "You are a root user"
+fi
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+VALIDATE $? "Downloading erlang repos for RabbitMQ"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+VALIDATE $? "Downlaoding RabbitMQ repos"
+
+dnf install rabbitmq-server -y
+
+VALIDATE $? "Installing RabbitMQ Server"
+
+systemctl enable rabbitmq-server
+
+VALIDATE $? "Enabling RabbitMQ Server"
+
+systemctl start rabbitmq-server
+
+VALIDATE $? "Starting RabbitMQ Server"
+
+rabbitmqctl add_user roboshop roboshop123
+
+VALIDATE $? "Adding RabbitMQ User and password"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+VALIDATE $? "Setting Permissions"
