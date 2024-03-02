050bc7ada44a02bcf20af18ed13e054d5883d56d
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-03-02 23:40:01 +0530

    Updated roboshop.sh file

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0e3316d7c9d69c2ad
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
-for i in "${INSTANCES[@]}"
+for i in ${INSTANCES[@]};
 do 
     echo "instance is : $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
